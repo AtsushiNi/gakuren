174167192069724c0c3d9543e0f68fbfdbe0e572
Atsushi <atsushi9731@gmail.com>
2019-03-16 02:12:08 +0900

    complete competent page

--- app/controllers/colleges_controller.rb
+++ app/controllers/colleges_controller.rb
@@ -1,6 +1,19 @@
 class CollegesController < ApplicationController
+  # before_action :require_college_login, only: [:competent_edit, :competent_entry]
+
   def index
     @colleges = College.all
+  end
+
+  def update
+    @college = College.find(params[:id])
+    if @college.update_attributes(college_params)
+      flash[:success] = "updated successfully"
+      redirect_to "/colleges/#{@college.id}/competent/edit"
+    else
+      p @college.errors
+      redirect_to competent_path
+    end
   end
 
   def competent_edit
@@ -9,5 +22,18 @@ class CollegesController < ApplicationController
 
   def competent_entry
     @college = College.find(params[:id])
+  end
+
+
+  private
+
+  def require_college_login
+    unless logged_in_college?
+      redirect_to login_path
+    end
+  end
+
+  def college_params
+    params.require(:college).permit(:players_num, :email, :captain, :competent)
   end
 end

4f8e294afd430a7c6a1611bf919cab45fb07206d
Atsushi <atsushi9731@gmail.com>
2019-03-25 16:38:04 +0900

    edit migrate for heroku

--- db/migrate/20190315223321_create_matches.rb
+++ db/migrate/20190315223321_create_matches.rb
@@ -3,10 +3,10 @@ class CreateMatches < ActiveRecord::Migration[5.2]
     create_table :matches do |t|
 
       t.references :singles_tournament
-      t.integer :young, array: true
-      t.integer :old, array: true
+      t.integer :young
+      t.integer :old
       t.integer :winner_num
-      t.integer :score, array: true
+      t.integer :score
       t.integer :parent_id
       t.timestamps
     end
